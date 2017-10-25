58395fdf7eadf78929abe5b338b5c7007672247d
CourageSuo <suoyong1000@gmail.com>
2017-10-25 19:36:52 +0800

    random 16

--- inta/routes/weixin.js
+++ inta/routes/weixin.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var https = require('https')
 var sha1 = require('sha1')
+var rs = require('randomstring')
 var router = express.Router();
+
 
 var url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx41719e0c25766535&secret=93af0348d0c1c658eb004c57e6008af8"
 
@@ -15,7 +17,7 @@ router.get("/",function(req,res){
 			https.get(jsUrl,function(res2){
 				res2.on('data',function(chunck){
 					var timeStamp = Math.floor(Date.now() / 1000)
-					var ranString = Math.random().toString(36).substring(2,12);
+					var ranString = rs.generater(16)
 					var js_ticket = JSON.parse(chunck.toString())
 					var string1 = "jsapi_ticket="+js_ticket.ticket+"&noncestr="+ranString+"&timestamp="+timeStamp+"&url=http://www.intalesson.com"
 					console.log(string1)
