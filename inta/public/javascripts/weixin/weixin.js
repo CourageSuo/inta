2a8fcba55dcc89337584a16353b9b1d071aa164b
CourageSuo <suoyong1000@gmail.com>
2017-10-28 08:29:25 +0800

    abs 300

--- inta/public/javascripts/weixin/weixin.js
+++ inta/public/javascripts/weixin/weixin.js
@@ -60,30 +60,16 @@ wx.ready(function(){
     title: 'INTA课程', // 分享标题
     link: 'http://www.intalesson.com/', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
     imgUrl: '/wechat.jpg', // 分享图标
+    trigger:function(){
+    },
     success: function () { 
         // 用户确认分享后执行的回调函数
-        console.log('c')
     },
     cancel: function () { 
         // 用户取消分享后执行的回调函数
     }
     });
-    wx.checkJsApi({
-    jsApiList: ['chooseImage'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-    success: function(res) {
-        console.log(res)
-        // 以键值对的形式返回，可用的api值true，不可用为false
-        // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-    }
-    });
-
 })
-wx.error(function(res){
-    console.log('b')
-});
 
 
 }
-
-
-
